61cfcc13dcd9dce09f5a61e24e960ce6c73e0d0d
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-10-09 16:35:05 +0200

    Add project to GitHub

---
+++ src/main/java/VoyageMate/VoyageMateApp/VoyageMateAppApplication.java
@@ -0,0 +1,13 @@
+package VoyageMate.VoyageMateApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageMateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyageMateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VoyageMate/VoyageMateApp/VoyageMateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package VoyageMate.VoyageMateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageMateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
